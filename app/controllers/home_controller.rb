50979df683021e93380053619e610fd83e5800a8
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-06-09 09:21:12 -0400

    Tuesday

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
 
   def form
-    
+    @result = Result.new(name: params[:name], chocolate: params[:chocolate], rainbows: params[:rainbows], cash: params[:cash])
+  end
+
+  def new
+
   end
 
 end
---
+++ db/migrate/20150609131003_create_results.rb
@@ -0,0 +1,13 @@
+class CreateResults < ActiveRecord::Migration
+  def change
+    create_table :results do |t|
+      t.string :name
+      t.boolean :chocolate
+      t.boolean :rainbows
+      t.boolean :puppies
+      t.boolean :cash
+
+      t.timestamps null: false
+    end
+  end
+end
